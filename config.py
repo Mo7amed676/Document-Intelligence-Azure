5a3f4ee02962bbaaad53f579149d6d972c28fbfc
MariamElwakel <mariamelwakel23@gmail.com>
2026-04-10 06:07:19 +0200

    Add configuration

---
+++ config.py
@@ -0,0 +1,20 @@
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.documentintelligence import DocumentIntelligenceClient
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+endpoint = os.getenv("ENDPOINT")
+key = os.getenv("KEY")
+
+
+def get_client() -> DocumentIntelligenceClient:
+    """
+    Create and return an Azure Document Intelligence client.
+    """
+    return DocumentIntelligenceClient(
+        endpoint=endpoint,
+        credential=AzureKeyCredential(key),
+    )
