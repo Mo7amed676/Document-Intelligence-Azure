6d9f45d4cdee9ff9d358756090e984d81e70c6e2
Mo7amed676 <84755046+Mo7amed676@users.noreply.github.com>
2026-04-14 12:03:26 +0200

    Add import for render_invoice_results function

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
 from layout_model import analyze_layout
-from output_format import render_layout_results,render_receipt_results
+from output_format import render_layout_results,render_receipt_results,render_invoice_results
 from general_document_model import analyze_general
 from ocr_model import analyze_ocr
 from custom_model import custom_model_app
 from receipt_model import analyze_receipt
+from invoice_model import analyze_invoice
 
 def file_uploader(label="Upload a document (PDF, JPEG, PNG, TIFF, BMP)"):
     """Single-file uploader used by most models."""
